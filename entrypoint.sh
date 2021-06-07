36900a429464e440f8a39e966a653bf4611b3b84
begyyal <ololo@ololo.ll>
2021-06-07 19:07:58 +0900

    初版

---
+++ entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -l
+
+token=$1
+url=$2
+method=$3
+arg=$4
+
+[ $url =~ ^https://api.github.com/ ] || return -1
+
+curl \
+  -X $method \
+  -H "Authorization: token $token" \
+  -H "Accept: application/vnd.github.v3+json" \
+  $url \
+  -d $arg
